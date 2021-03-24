0c3a27f3cd432124d0b92599f33cbce2892b2de2
Steen Jørgensen <stjo2809@gmail.com>
2021-03-24 16:57:58 +0100

    Fresh new start to the Solution and Project
    Next setup identity dbcontext

---
+++ OrderLogisticsManagerApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderLogisticsManagerApplication.Areas.Identity.Data;
+using OrderLogisticsManagerApplication.Data;
+
+[assembly: HostingStartup(typeof(OrderLogisticsManagerApplication.Areas.Identity.IdentityHostingStartup))]
+namespace OrderLogisticsManagerApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ApplicationIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ApplicationIdentityContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationIdentityContext>();
+            });
+        }
+    }
+}
