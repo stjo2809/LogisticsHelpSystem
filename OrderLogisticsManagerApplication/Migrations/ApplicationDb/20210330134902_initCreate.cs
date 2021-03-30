2914297372e54bd46eb643669095e2c899aa547c
Steen Jørgensen <stjo2809@gmail.com>
2021-03-30 15:52:28 +0200

    Added EF Core Migration initCreate on both

---
+++ OrderLogisticsManagerApplication/Migrations/ApplicationDb/20210330134902_initCreate.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderLogisticsManagerApplication.Migrations.ApplicationDb
+{
+    public partial class initCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Components",
+                columns: table => new
+                {
+                    ComponentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComponentPartNumber = table.Column<int>(type: "int", nullable: false),
+                    ComponentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ComponentWidth = table.Column<float>(type: "real", nullable: false),
+                    ComponentHeigth = table.Column<float>(type: "real", nullable: false),
+                    ComponentDepth = table.Column<float>(type: "real", nullable: false),
+                    ComponentWeigth = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Components", x => x.ComponentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackingMaterials",
+                columns: table => new
+                {
+                    MaterialID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaterialPartNumber = table.Column<int>(type: "int", nullable: false),
+                    MaterialName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HasDimension = table.Column<bool>(type: "bit", nullable: false),
+                    MaterialWidth = table.Column<float>(type: "real", nullable: false),
+                    MaterialHeigth = table.Column<float>(type: "real", nullable: false),
+                    MaterialDepth = table.Column<float>(type: "real", nullable: false),
+                    MaterialWeigth = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackingMaterials", x => x.MaterialID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserGUID = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Logs",
+                columns: table => new
+                {
+                    LogID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LogTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LogAction = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogOn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LogDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logs", x => x.LogID);
+                    table.ForeignKey(
+                        name: "FK_Logs_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderNumber = table.Column<int>(type: "int", nullable: false),
+                    OrderFeedbackNumber = table.Column<int>(type: "int", nullable: false),
+                    OrderAmount = table.Column<int>(type: "int", nullable: false),
+                    ComponentID = table.Column<int>(type: "int", nullable: true),
+                    OrderStartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderEndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    OrderEnteredByUserID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Components_ComponentID",
+                        column: x => x.ComponentID,
+                        principalTable: "Components",
+                        principalColumn: "ComponentID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_OrderEnteredByUserID",
+                        column: x => x.OrderEnteredByUserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pickups",
+                columns: table => new
+                {
+                    PickupID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: true),
+                    PickupTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pickups", x => x.PickupID);
+                    table.ForeignKey(
+                        name: "FK_Pickups_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deliveries",
+                columns: table => new
+                {
+                    DeliveryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: true),
+                    DeliveryAmount = table.Column<int>(type: "int", nullable: false),
+                    DeliveryTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deliveries", x => x.DeliveryID);
+                    table.ForeignKey(
+                        name: "FK_Deliveries_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Deliveries_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackingMaterialUsedOnOrders",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    MaterialID = table.Column<int>(type: "int", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackingMaterialUsedOnOrders", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PackingMaterialUsedOnOrders_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PackingMaterialUsedOnOrders_PackingMaterials_MaterialID",
+                        column: x => x.MaterialID,
+                        principalTable: "PackingMaterials",
+                        principalColumn: "MaterialID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PickupRequests",
+                columns: table => new
+                {
+                    PickupRequestID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: true),
+                    PickupRequestAmount = table.Column<int>(type: "int", nullable: false),
+                    PickupRequestTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PickupID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickupRequests", x => x.PickupRequestID);
+                    table.ForeignKey(
+                        name: "FK_PickupRequests_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PickupRequests_Pickups_PickupID",
+                        column: x => x.PickupID,
+                        principalTable: "Pickups",
+                        principalColumn: "PickupID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PickupRequests_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_OrderID",
+                table: "Deliveries",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_UserID",
+                table: "Deliveries",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Logs_UserID",
+                table: "Logs",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ComponentID",
+                table: "Orders",
+                column: "ComponentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderEnteredByUserID",
+                table: "Orders",
+                column: "OrderEnteredByUserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackingMaterialUsedOnOrders_MaterialID",
+                table: "PackingMaterialUsedOnOrders",
+                column: "MaterialID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackingMaterialUsedOnOrders_OrderID",
+                table: "PackingMaterialUsedOnOrders",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PickupRequests_OrderID",
+                table: "PickupRequests",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PickupRequests_PickupID",
+                table: "PickupRequests",
+                column: "PickupID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PickupRequests_UserID",
+                table: "PickupRequests",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pickups_UserID",
+                table: "Pickups",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deliveries");
+
+            migrationBuilder.DropTable(
+                name: "Logs");
+
+            migrationBuilder.DropTable(
+                name: "PackingMaterialUsedOnOrders");
+
+            migrationBuilder.DropTable(
+                name: "PickupRequests");
+
+            migrationBuilder.DropTable(
+                name: "PackingMaterials");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Pickups");
+
+            migrationBuilder.DropTable(
+                name: "Components");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
