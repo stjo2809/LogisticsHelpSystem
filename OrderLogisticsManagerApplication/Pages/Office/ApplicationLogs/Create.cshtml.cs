e8ae5bd5306fd00200c5fb4c62f0f7e7b4c4a6e3
Steen Jørgensen <stjo2809@gmail.com>
2021-04-12 19:32:46 +0200

    change name logs to applicationlogs to add to git

---
+++ OrderLogisticsManagerApplication/Pages/Office/ApplicationLogs/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OrderLogisticsManagerApplication.Models.Database.ApplicationDb;
+
+namespace OrderLogisticsManagerApplication.Pages.Office.Logs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly OrderLogisticsManagerApplication.Models.Database.ApplicationDb.ApplicationDbContext _context;
+
+        public CreateModel(OrderLogisticsManagerApplication.Models.Database.ApplicationDb.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Log Log { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Logs.Add(Log);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ OrderLogisticsManagerApplication/Pages/Office/ApplicationLogs/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OrderLogisticsManagerApplication.Models.Database.ApplicationDb;
+
+namespace OrderLogisticsManagerApplication.Pages.Office.Logs
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly OrderLogisticsManagerApplication.Models.Database.ApplicationDb.ApplicationDbContext _context;
+
+        public DeleteModel(OrderLogisticsManagerApplication.Models.Database.ApplicationDb.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Log Log { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Log = await _context.Logs.FirstOrDefaultAsync(m => m.LogID == id);
+
+            if (Log == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Log = await _context.Logs.FindAsync(id);
+
+            if (Log != null)
+            {
+                _context.Logs.Remove(Log);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
