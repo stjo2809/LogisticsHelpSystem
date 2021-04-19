389212b5e38f69fc162b756a302477866ba5dc1d
Steen Jørgensen <stjo2809@gmail.com>
2021-04-19 20:38:40 +0200

    start on warehouse

--- OrderLogisticsManagerApplication/Pages/Warehouse/Index.cshtml.cs
+++ OrderLogisticsManagerApplication/Pages/Warehouse/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LogisticsHelpSystemLibrary.Models.Database.ApplicationDb;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,18 @@ namespace OrderLogisticsManagerApplication.Pages.Warehouse
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext applicationDbContext;
+
+
+
+        public IndexModel(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
         public void OnGet()
         {
+            // linq group by
         }
     }
 }
