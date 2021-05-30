9f53e9a994bb2d46adf24fc8666cbf75bf8645f3
Steen Jørgensen <stjo2809@gmail.com>
2021-05-30 12:23:20 +0200

    Updated Seeddata

--- OrderLogisticsManagerApplication/Areas/Api/Controllers/SeedDataController.cs
+++ OrderLogisticsManagerApplication/Areas/Api/Controllers/SeedDataController.cs
@@ -39,14 +39,14 @@ namespace OrderLogisticsManagerApplication.Areas.Api.Controllers
             applicationDbContext.SaveChanges();
 
             #region CardStatus
-            applicationDbContext.CardStatuses.Add(new CardStatus() { StatusDescription = "Active" });
+                applicationDbContext.CardStatuses.Add(new CardStatus() { StatusDescription = "Active" });
                 applicationDbContext.CardStatuses.Add(new CardStatus() { StatusDescription = "Inactive" });
             #endregion
 
             applicationDbContext.SaveChanges();
 
             #region Priority
-            applicationDbContext.Priorities.Add(new Priority() { Description = "High" });
+                applicationDbContext.Priorities.Add(new Priority() { Description = "High" });
                 applicationDbContext.Priorities.Add(new Priority() { Description = "Normal" });
                 applicationDbContext.Priorities.Add(new Priority() { Description = "Low" });
             #endregion
@@ -54,7 +54,7 @@ namespace OrderLogisticsManagerApplication.Areas.Api.Controllers
             applicationDbContext.SaveChanges();
 
             #region Component
-            applicationDbContext.Components.Add(new Component()
+                applicationDbContext.Components.Add(new Component()
                 {
                     ComponentPartNumber = "100",
                     ComponentName = "Radio",
@@ -81,12 +81,30 @@ namespace OrderLogisticsManagerApplication.Areas.Api.Controllers
                     ComponentDepth = (float)15,
                     ComponentWeigth = (float)5,
                 });
+                applicationDbContext.Components.Add(new Component()
+                {
+                    ComponentPartNumber = "400",
+                    ComponentName = "BremseComputer",
+                    ComponentHeigth = (float)20,
+                    ComponentWidth = (float)60,
+                    ComponentDepth = (float)30,
+                    ComponentWeigth = (float)3,
+                });
+                applicationDbContext.Components.Add(new Component()
+                {
+                    ComponentPartNumber = "500",
+                    ComponentName = "FBC TogComputer",
+                    ComponentHeigth = (float)10,
+                    ComponentWidth = (float)40,
+                    ComponentDepth = (float)40,
+                    ComponentWeigth = (float)2.3,
+                });
             #endregion
 
             applicationDbContext.SaveChanges();
 
             #region PackingMaterial
-            applicationDbContext.PackingMaterials.Add(new PackingMaterial()
+                applicationDbContext.PackingMaterials.Add(new PackingMaterial()
                 {
                     MaterialPartNumber = "400",
                     MaterialName = "PapKasse -> TestMateriale",
@@ -108,7 +126,7 @@ namespace OrderLogisticsManagerApplication.Areas.Api.Controllers
             applicationDbContext.SaveChanges();
 
             #region  WorkGroup
-            applicationDbContext.WorkGroups.Add(new WorkGroup()
+                applicationDbContext.WorkGroups.Add(new WorkGroup()
                 {
                     WorkGroupNumber = "100-120",
                     WorkGroupName = "Location A"
@@ -138,17 +156,116 @@ namespace OrderLogisticsManagerApplication.Areas.Api.Controllers
             applicationDbContext.SaveChanges();
 
             #region Order
-            applicationDbContext.Orders.Add(new Order()
-            {
-                OrderNumber = "PO100",
-                OrderFeedbackNumber = "FB100",
-                OrderAmount = 2,
-                OrderStartDate = DateTime.Now,
-                OrderEndDate = DateTime.Now,
-                Priority = applicationDbContext.Priorities.FirstOrDefault(),
-                WorkGroup = applicationDbContext.WorkGroups.FirstOrDefault(),
-                Component = applicationDbContext.Components.FirstOrDefault()
-            });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO100",
+                    OrderFeedbackNumber = "FB100",
+                    OrderAmount = 2,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(10),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "High").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-120").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "100").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO200",
+                    OrderFeedbackNumber = "FB200",
+                    OrderAmount = 1,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(5),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-120").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "200").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO300",
+                    OrderFeedbackNumber = "FB300",
+                    OrderAmount = 5,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(25),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-130").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "300").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO400",
+                    OrderFeedbackNumber = "FB400",
+                    OrderAmount = 1,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(5),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-130").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "400").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO500",
+                    OrderFeedbackNumber = "FB500",
+                    OrderAmount = 3,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(15),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Low").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-140").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "500").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO600",
+                    OrderFeedbackNumber = "FB600",
+                    OrderAmount = 4,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(20),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-140").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "100").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO700",
+                    OrderFeedbackNumber = "FB700",
+                    OrderAmount = 1,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(5),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "High").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-150").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "200").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO800",
+                    OrderFeedbackNumber = "FB800",
+                    OrderAmount = 2,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(10),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-150").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "300").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO900",
+                    OrderFeedbackNumber = "FB900",
+                    OrderAmount = 1,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(5),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-160").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "400").FirstOrDefault()
+                });
+                applicationDbContext.Orders.Add(new Order()
+                {
+                    OrderNumber = "PO1000",
+                    OrderFeedbackNumber = "FB1000",
+                    OrderAmount = 1,
+                    OrderStartDate = DateTime.Now,
+                    OrderEndDate = DateTime.Now.AddDays(5),
+                    Priority = applicationDbContext.Priorities.Where(x => x.Description == "Normal").FirstOrDefault(),
+                    WorkGroup = applicationDbContext.WorkGroups.Where(x => x.WorkGroupNumber == "100-160").FirstOrDefault(),
+                    Component = applicationDbContext.Components.Where(x => x.ComponentPartNumber == "500").FirstOrDefault()
+                });
             #endregion
 
             applicationDbContext.SaveChanges();
@@ -178,14 +295,57 @@ namespace OrderLogisticsManagerApplication.Areas.Api.Controllers
                 applicationDbContext.SaveChanges();
             #endregion
 
+            applicationDbContext.SaveChanges();
+
             #region  card
-                applicationDbContext.Card.Add(new Card()
+            applicationDbContext.Card.Add(new Card()
                 {
                     CardNumber = "1234567890",
                     Status = applicationDbContext.CardStatuses.Where(x => x.StatusDescription == "Active").FirstOrDefault(),
                     User = applicationDbContext.Users.Where(x => x.FirstName == "Test").FirstOrDefault(),
                 });
             #endregion
+
+            applicationDbContext.SaveChanges();
+
+            #region Deliveries
+            foreach (var item in applicationDbContext.Orders.ToList())
+            {
+                if (item.OrderNumber == "PO1000")
+                continue;
+
+                applicationDbContext.Deliveries.Add(new Delivery()
+                {
+                    DeliveryAmount = item.OrderAmount,
+                    DeliveryTime = DateTime.Now,
+                    OrderID = item.OrderID,
+                    UserID = applicationDbContext.Users.FirstOrDefault().UserID
+                });
+            }
+            #endregion
+
+            applicationDbContext.SaveChanges();
+
+            #region PickupRequests
+            var orderlist = applicationDbContext.Orders.ToList();
+
+            for (int i = 0; i < orderlist.Count; i++)
+            {
+                if (i % 2 != 0)
+                    continue;
+
+                applicationDbContext.PickupRequests.Add(new PickupRequest()
+                {
+                    OrderID = orderlist[i].OrderID,
+                    PickupRequestAmount = orderlist[i].OrderAmount,
+                    PickupRequestTime = DateTime.Now,
+                    UserID = applicationDbContext.Users.FirstOrDefault().UserID
+                });
+            };
+            
+            #endregion
+
+            applicationDbContext.SaveChanges();
 
             // Log Seeding
             applicationDbContext.Logs.Add(new Log() 
